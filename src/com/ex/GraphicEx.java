bbe5d29f3928a55d39217ba993a8b2b33c3d9ea3
kangmyungseok <kangmyungseok@218.38.137.28>
2021-06-02 14:16:54 +0900

    this is graphic

--- src/com/ex/GraphicEx.java
+++ src/com/ex/GraphicEx.java
@@ -1,7 +1,6 @@
 package com.ex;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class GraphicEx extends Frame implements ActionListener {
 	
@@ -13,7 +12,7 @@ public class GraphicEx extends Frame implements ActionListener {
 	
 	
 	public GraphicEx() {
-		
+		super("GraphicEx");
 		MenuBar mb = new MenuBar();
 		Menu menu_tool = new Menu("도구");
 		
@@ -27,18 +26,102 @@ public class GraphicEx extends Frame implements ActionListener {
 		menu_tool.add(menu_tool_circle);
 		menu_tool.add(menu_tool_rect);
 		
+		mb.add(menu_tool);
+		setMenuBar(mb);
+		
+		setBounds(200, 200, 400, 400);
+		setVisible(true);
+		WinEvent we = new WinEvent();
+		addWindowListener(we);
+		
+		menu_tool_pen.addActionListener(this);
+		menu_tool_line.addActionListener(this);
+		menu_tool_circle.addActionListener(this);
+		menu_tool_rect.addActionListener(this);
 	}
 	
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
+	public void actionPerformed(ActionEvent e) {
+		MenuItem selected_menu = (MenuItem)e.getSource();
+		
+		if(selected_menu.equals(menu_tool_pen)) {
+			str=menu_tool_pen.getLabel();
+		}else if(selected_menu.equals(menu_tool_line)) {
+			str=menu_tool_line.getLabel();
+		}else if(selected_menu.equals(menu_tool_circle)) {
+			str=menu_tool_circle.getLabel();
+		}else if(selected_menu.equals(menu_tool_rect)) {
+			str=menu_tool_rect.getLabel();
+		}
+			
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new GraphicEx();
 
 	}
 
+	class GrimPanCanvas extends Canvas implements MouseMotionListener, MouseListener{
+
+		
+		public GrimPanCanvas() {
+		
+		
+		}
+		
+		@Override
+		public void paint(Graphics g) {
+		
+		}
+		
+		@Override
+		public void update(Graphics g) {
+		
+		}
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 }
